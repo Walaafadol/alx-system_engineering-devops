af181788ba004d84dadc0e3acb1025ba20c93456
Walaafadol <walaafadul7@gmail.com>
2024-02-23 13:13:09 -0800

    kk

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#create file
+# manifest to create file in tmp directory
 file { '/tmp/school':
-	path    => '/tmp/school',
-	content	=> 'I love Puppet',
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group   => 'www-data'
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
