b52dd3b155b04663c35fdd0b414399aaec50f00f
Walaafadol <walaafadul7@gmail.com>
2024-02-23 13:06:33 -0800

    kk

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #create file
 file { '/tmp/school':
-	ensure  => 'file',
 	path    => '/tmp/school',
 	content	=> 'I love Puppet',
 	mode	=> '0744',
 	owner	=> 'www-data',
-	group   => 'www-data',
+	group   => 'www-data'
 }
